4c9a1a5aba0e39d2542cc1d98f1e3185730c49c0
jominkook <whaksenaos@naver.com>
2025-03-24 17:12:55 +0900

    코딩테스트 자바 4일차

--- src/main/java/Solved_1012_11.java
+++ src/main/java/Solved_1012_11.java
@@ -2,24 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Sovled_1012_10 {
+public class Solved_1012_11 {
     public static int count = 0;
     public static int [][] graph;
     public static boolean [][] visited;
     public static int [] dx = {-1,1,0,0};
     public static int [] dy = {0,0,-1,1};
 
+
     public static void main(String [] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        for(int i = 0; i<T; i++){
-            String [] buffer = br.readLine().split(" ");
-            int M = Integer.parseInt(buffer[0]);
-            int N = Integer.parseInt(buffer[1]);
-            int K = Integer.parseInt(buffer[2]);
 
-            graph = new int [N][M];
+        for(int i = 0; i<T; i++){
+            String [] input = br.readLine().split(" ");
+            int M = Integer.parseInt(input[0]);
+            int N = Integer.parseInt(input[1]);
+            int K = Integer.parseInt(input[2]);
+
+            graph = new int[N][M];
             visited = new boolean[N][M];
 
             for(int j = 0; j<K; j++){
@@ -28,13 +30,10 @@ public class Sovled_1012_10 {
                 int Y = Integer.parseInt(pos[1]);
 
                 graph[Y][X] = 1;
-
-
             }
-
             count = 0;
 
-            for(int j = 0; j< N; j++){
+            for(int j = 0; j<N; i++){
                 for(int k = 0; k<M; k++){
                     if(graph[j][k] == 1 && !visited[j][k]){
                         dfs(j,k,N,M);
@@ -45,13 +44,16 @@ public class Sovled_1012_10 {
             System.out.println(count);
         }
     }
-    public static void dfs(int x, int y, int N, int M){
+    private static void dfs(int x, int y , int N, int M){
         visited[x][y] = true;
 
-        for(int i = 0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && graph[nx][ny] == 1 && !visited[nx][ny]) {
+                dfs(nx, ny, N, M);
+            }
         }
     }
 }
---
+++ src/main/java/Solved_2606_3.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solved_2606_3 {
+    public static int count = 0;
+    public static boolean [] visited;
+
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int com = Integer.parseInt(br.readLine());
+        int connections = Integer.parseInt(br.readLine());
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        visited = new boolean[com+1];
+
+
+        for(int i = 0; i<=com; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i<connections; i++){
+            String [] pos = br.readLine().split(" ");
+            int a = Integer.parseInt(pos[0]);
+            int b = Integer.parseInt(pos[1]);
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+
+        }
+        dfs(list,1,visited);
+        System.out.println(count - 1);
+
+
+    }
+    private static void dfs(ArrayList<ArrayList<Integer>> graph, int start, boolean [] visited){
+        visited[start] = true;
+
+        count += 1;
+        for(int i : graph.get(start)){
+            if(!visited[i]){
+                dfs(graph,i,visited);
+            }
+        }
+    }
+}
