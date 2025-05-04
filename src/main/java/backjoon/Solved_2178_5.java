4c0a984acd8011aea4a35344eb3b4ad6b098697a
jominkook <whaksenaos@naver.com>
2025-05-05 00:23:54 +0900

    코딩테스트 자바 24일차-2

--- src/main/java/backjoon/Solved_2178_5.java
+++ src/main/java/backjoon/Solved_2178_5.java
@@ -6,14 +6,15 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class Solved_2178_3 {
-    static int [] dx = {-1,1,0,0};
-    static int [] dy = {0,0,-1,1};
+public class Solved_2178_5 {
     static int [][] graph;
     static boolean [][] visited;
-    static int M,N,nx,ny;
-    public static void main(String [] args) throws IOException {
+    static int [] dx = {-1,1,0,0};
+    static int [] dy = {0,0,-1,1};
+    static int M,N;
+    public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String [] buffer = br.readLine().split(" ");
 
         N = Integer.parseInt(buffer[0]);
@@ -22,34 +23,33 @@ public class Solved_2178_3 {
         graph = new int[N][M];
         visited = new boolean[N][M];
 
-
         for(int i = 0; i<N; i++){
             String input = br.readLine();
             for(int j = 0; j<M; j++){
                 graph[i][j] = input.charAt(j) - '0';
             }
         }
-        System.out.println(bfs(graph,0,0,visited));
 
+        System.out.println(bfs(graph,0,0,visited));
     }
-    private static int bfs(int [][] graph,int x ,int y,boolean[][] visited){
+    private static int bfs(int [][] graph,int x, int y, boolean[][] visited){
         Deque<int []> deque = new ArrayDeque<>();
         visited[x][y] = true;
         deque.add(new int[]{x,y});
 
         while (!deque.isEmpty()){
             int [] curr = deque.poll();
-
-            int curX = curr[0];
-            int curY = curr[1];
+            int posX = curr[0];
+            int posY = curr[1];
 
             for(int i = 0; i<4; i++){
-                nx = curX + dx[i];
-                ny = curY + dy[i];
-                if(nx >= 0 && nx < N && ny >= 0 && ny < M){
+                int nx = posX + dx[i];
+                int ny = posY + dy[i];
+
+                if(nx >=0 && nx < N && ny >= 0 && ny <M){
                     if(graph[nx][ny] == 1 && !visited[nx][ny]){
                         deque.add(new int[]{nx,ny});
-                        graph[nx][ny] = graph[nx][ny] + graph[curX][curY];
+                        graph[nx][ny] += graph[posX][posY];
                         visited[nx][ny] = true;
                     }
                 }
