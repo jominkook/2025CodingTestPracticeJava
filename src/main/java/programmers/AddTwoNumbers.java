0867eeb4eb485c1d707569ce791f30805dbf3fdd
jominkook <whaksenaos@naver.com>
2025-06-06 20:13:33 +0900

    코딩테스트 36일차

--- src/main/java/programmers/AddTwoNumbers.java
+++ src/main/java/programmers/AddTwoNumbers.java
@@ -1,4 +1,25 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class AddTwoNumbers {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i<numbers.length; i++){
+            for(int j = i+1; j<numbers.length; j++){
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        //System.out.println(set);
+        int[] answer = {};
+
+        answer = set.stream().mapToInt(Integer::intValue).toArray();
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
 }
