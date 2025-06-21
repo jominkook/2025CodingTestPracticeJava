b793a821500f0d78fdece2ff58e06dd87b3f1b59
jominkook <whalsrnraos@naver.com>
2025-06-21 18:02:38 +0900

    자바코딩테스트 47일차

---
+++ src/main/java/backjoon/Solved_2531_4.java
@@ -0,0 +1,24 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solved_2531_4 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] buffer = br.readLine().split(" ");
+        int n = Integer.parseInt(buffer[0]);
+        int d = Integer.parseInt(buffer[1]);
+        int k = Integer.parseInt(buffer[2]);
+        int c = Integer.parseInt(buffer[3]);
+
+        int [] sushi = new int[n+k-1];
+
+        
+
+
+
+
+    }
+}
