3f6de2c843f922fb982cd84854b83f76e848de15
jominkook <whalsrnraos@naver.com>
2025-08-30 21:40:13 +0900

    자바 코딩테스트 65일차

--- src/main/java/backjoon/Solved_17266_8.java
+++ src/main/java/backjoon/Solved_17266_8.java
@@ -4,47 +4,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solved_17266_5 {
+public class Solved_17266_8 {
     public static void main(String [] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
         int M = Integer.parseInt(br.readLine());
 
         int [] lights = new int[M];
-
         String [] buffer = br.readLine().split(" ");
-        for(int i = 0; i < M; i++){
+        for(int i = 0; i<M; i++){
             lights[i] = Integer.parseInt(buffer[i]);
         }
 
         int maxDistance = 0;
 
+
         if(M == 1){
             System.out.println(Math.max(lights[0], N - lights[0]));
         }else{
-            for(int i = 0; i < M; i++){
+            for(int i = 0; i<M; i++){
                 if(i == 0){
                     maxDistance = lights[i];
-                }else if(i == M - 1){
+                }else if(i == M-1){
                     maxDistance = Math.max(maxDistance, N - lights[i]);
                 }else{
                     int distance = lights[i] - lights[i-1];
 
-                    if(distance % 2 == 0){
-                        distance = distance / 2;
-                    }else if(distance % 2 == 1){
-                        distance = distance / 2 + 1;
+                    if(distance % 2 == 1){
+                        maxDistance = distance/2 + 1;
+                    }else{
+                        maxDistance = distance/2;
                     }
-                    maxDistance = Math.max(maxDistance, distance);
-
-
+                    maxDistance = Math.max(maxDistance,distance);
                 }
             }
             System.out.println(maxDistance);
         }
-
-
     }
 }
