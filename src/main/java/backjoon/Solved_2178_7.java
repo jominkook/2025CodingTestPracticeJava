662f27e66d95cbcd2c2acc2ab993497c59b627ee
jominkook <whalsrnraos@naver.com>
2025-09-12 00:30:21 +0900

    자바 코딩테스트 72일차

---
+++ src/main/java/backjoon/Solved_2178_7.java
@@ -0,0 +1,69 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solved_2178_7 {
+    static int count = 0;
+    static int N,M;
+    static int [] dx = {-1,1,0,0};
+    static int [] dy = {0,0,-1,1};
+    static int [][] graph;
+    static boolean [][] visited;
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] buffer = br.readLine().split(" ");
+
+        N = Integer.parseInt(buffer[0]);
+        M = Integer.parseInt(buffer[1]);
+
+        graph = new int[N][M];
+        visited = new boolean[N][M];
+
+        for(int i=0;i<N;i++){
+            String line = br.readLine();
+            for(int j=0;j<M;j++){
+                graph[i][j] = line.charAt(j)-'0';
+            }
+        }
+
+        System.out.println(bfs(graph,0,0,visited));
+
+//        for(int i=0;i<N;i++){
+//            for(int j=0;j<M;j++){
+//                System.out.println(graph[i][j]);
+//            }
+//        }
+
+
+    }
+    static int bfs(int [][] graph,int x,int y,boolean[][]visited){
+        Deque<int [] > queue = new LinkedList<>();
+        visited[x][y]=true;
+        queue.add(new int[]{x,y});
+
+        while(!queue.isEmpty()){
+            int [] cur = queue.poll();
+            int curX = cur[0];
+            int curY = cur[1];
+
+            for(int i=0;i<4;i++){
+                int nx = curX+dx[i];
+                int ny = curY+dy[i];
+
+                if(nx>=0 && ny>=0 && nx<N && ny<M){
+                    if(!visited[nx][ny] && graph[nx][ny]==1){
+                        queue.add(new int[]{nx,ny});
+                        graph[nx][ny] += graph[curX][curY];
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+        return graph[N-1][M-1];
+    }
+}
