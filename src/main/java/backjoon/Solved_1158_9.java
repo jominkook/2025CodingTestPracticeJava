893839d96a9cf88d30af6b96125234beb255a0ba
jominkook <whalsrnraos@naver.com>
2025-11-27 00:41:07 +0900

    자바 코딩테스트 90일차

---
+++ src/main/java/backjoon/Solved_1158_9.java
@@ -0,0 +1,42 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solved_1158_9 {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] buffer = br.readLine().split(" ");
+        int N = Integer.parseInt(buffer[0]);
+        int K = Integer.parseInt(buffer[1]);
+
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i<=N; i++){
+            q.add(i);
+        }
+//        System.out.println(q);
+
+        while(!q.isEmpty()){
+            for(int i = 0; i<K-1; i++){
+                q.add(q.poll());
+            }
+            sb.append(q.poll());
+            if(!q.isEmpty()){
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+
+        System.out.println(sb);
+
+
+    }
+}
--- src/main/java/backjoon/Solved_20310_1.java
+++ src/main/java/backjoon/Solved_20310_1.java
@@ -3,9 +3,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+
 
 public class Solved_20310_1 {
     public static void main(String [] args) throws IOException {
