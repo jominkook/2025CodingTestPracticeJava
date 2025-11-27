9871cbdf3b3c70ba6c8f31e64901cbd96bc1ac95
jominkook <whalsrnraos@naver.com>
2025-11-27 23:06:12 +0900

    자바 코딩테스트 91일차

---
+++ src/main/java/backjoon/Solved_1158_10.java
@@ -0,0 +1,39 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solved_1158_10 {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] buffer = br.readLine().split(" ");
+
+        int N = Integer.parseInt(buffer[0]);
+        int K = Integer.parseInt(buffer[1]);
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll());
+            if(!queue.isEmpty()){
+                sb.append(", ");
+            }
+
+        }
+        sb.append(">");
+        System.out.println(sb);
+    }
+}
