014fe7f909f3a8d66a366686f1d7fef6a235cb00
jominkook <whalsrnraos@naver.com>
2025-12-07 18:54:35 +0900

    자바 코딩테스트 94일차

---
+++ src/main/java/backjoon/Solved_19941_12.java
@@ -0,0 +1,45 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solved_19941_12 {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] buffer = br.readLine().split(" ");
+
+        int N = Integer.parseInt(buffer[0]);
+        int K = Integer.parseInt(buffer[1]);
+        char [] table = new char[N];
+
+        String word = br.readLine();
+        for(int i = 0; i < N; i++){
+            table[i] = word.charAt(i);
+        }
+
+        //for (int i = 0; i < N; i++) {
+            //System.out.print(table[i] + " ");
+        //}
+        int cnt = 0;
+
+        for(int i = 0; i<table.length; i++){
+            if(table[i] == 'P'){
+                for(int j = i-K; j<i+K+1; j++){
+                    if(j < 0 || j >= N){
+                        continue;
+                    }
+                    if(table[j] == 'H'){
+                        table[j] = 'X';
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+
+
+    }
+}
---
+++ src/main/java/backjoon/Solved_2531_18.java
@@ -0,0 +1,60 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solved_2531_18 {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] buffer = br.readLine().split(" ");
+
+        int N = Integer.parseInt(buffer[0]);
+        int d = Integer.parseInt(buffer[1]);
+        int k = Integer.parseInt(buffer[2]);
+        int c = Integer.parseInt(buffer[3]);
+
+        int [] sushi = new int[N+k-1];
+
+        for(int i = 0; i<N; i++){
+            sushi[i] = Integer.parseInt(br.readLine());
+        }
+
+
+        int start = 0;
+        int max = 1;
+        int [] eaten = new int[d+1];
+        eaten[c] = eaten[c] + 1;
+
+        for(int i = 0; i<k-1; i++){
+            sushi[N++] = sushi[i];
+        }
+
+        for(int i = start; i<k; i++){
+            if(eaten[sushi[i]] == 0){
+                max++;
+            }
+            eaten[sushi[i]] = eaten[sushi[i]] + 1;
+        }
+
+        start = 0;
+        int result = max;
+        int end = k;
+
+        for(int i = end; i<sushi.length; i++){
+            eaten[sushi[start]] = eaten[sushi[start]] - 1;
+            if(eaten[sushi[start]] == 0){
+                result -= 1;
+            }
+
+            if(eaten[sushi[i]] == 0){
+                result += 1;
+            }
+            eaten[sushi[i]] = eaten[sushi[i]] + 1;
+            result = Math.max(max, result);
+            start++;
+        }
+        System.out.println(max);
+    }
+}
